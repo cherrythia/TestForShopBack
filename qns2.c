be0b90c6d419dcac45d30188bf36c1842c0feade
Terry Chia <chiawz@hotmail.com>
2015-05-31 08:41:17 +0800

    modified - macbook dropped at every tenth floor, then every five floor, then every single floor.

--- qns2.c
+++ qns2.c
@@ -1,53 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 
-//define the floor that the macbook pro will break
-int breaklvl = 22;
+/*----------------------Assumption----------------------------*/
+//1. The Macbook pro will break at breaklvl.
+//2. Macbook pro drop below breaklvl will not break. 
+//3. Macbook pro drop at breaklvl and above breaklvl will break.
 
-//Global variable
-int count = 0; 
+#define breaklvl 44
 
-//Singular testing = every single floor
-int singulardrop(test) 
-{
-	++count;
-	test -= 1;
-	if(test != breaklvl){
-		return singulardrop(test);
+/*----------------------Method-------------------------------*/
+//We test for the breaklvl at different intervals.
+//1. Test at every 10 level. 
+//2. Minus 5 level and test.
+//3. test at every single level. 
+
+//Singular level test at every floor
+int singular(test,count){
+	count++;
+	if (test< breaklvl)
+	{	
+		test +=1;
+		singular(test,count);
 	}
-
-	else{
-		printf("Breaking point is found!!\n");
+	else if (test > breaklvl)
+	{	
+		test -= 1;
+		singular(test,count);
+	}
+	else{ //when they are equal print this
+		printf("Breaking point is found!\n");
 		printf("Macbook1 breaks at = %d\n", test);
 		printf("Macbook2 survived at = %d\n", (test-1) );
 		printf("The number of times it has dropped %d\n", count);
 	}
-
 	return 1;
 }
 
 //Interval testing - every 10th floor
-int intervaldrop(test){
-		
+int intervaldrop(test,count){
 	++count;
 	
 	if(test < breaklvl)
 	{	
-		test += 10; 				//interval of tenth
-		return intervaldrop(test);
+		test += 10; 					
+		return intervaldrop(test,count);
 	}
 	else{
-		singulardrop(test);
+		count++;
+		test -= 5;					//minus 5 floors and test
+		singular(test,count);
 	}
-
 	return 1;
 }
 
 
 int main()
 {	
-	int test = 10; //test starts from the 10th floor
-	intervaldrop(test);
+	int count = 0;
+	int test = 10; 					//test starts from the 10th floor
+	intervaldrop(test,count);
 
 	return 0;
 }
